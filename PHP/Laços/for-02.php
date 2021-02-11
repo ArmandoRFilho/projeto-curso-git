a0d21eec8b96b1d389b806f8965bdcfe3c0aa91b
ArmandoRFilho <armando@mck2.com.br>
2021-02-11 20:02:29 -0300

    Adicionando Array

--- PHP/Laços/for-02.php
+++ PHP/Laços/for-02.php
@@ -1,6 +1,6 @@
 <?php
 
-echo "<select>";
+echo "<select>"; # essa é uma tag html que ira permitir o susário criar uma combo do tipo select com o parametro do laço for 
 
 for ($i=date("Y"); $i >= date("Y")-100; $i--) { 
     # testando o laço para regredir os anos
