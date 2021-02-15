10b238073e16e7e470ef6849bc8580629a028fd9
ArmandoRFilho <armando@mck2.com.br>
2021-02-15 09:50:03 -0300

    Date

---
+++ PHP/Date/data-02.php
@@ -0,0 +1,7 @@
+<?php
+
+//$ts = strtotime("2021-02-14");
+$ts = strtotime("Now");
+echo date("l, d/m/y", $ts);
+
+?>
---
+++ PHP/Date/data-03.php
@@ -0,0 +1,7 @@
+<?php
+
+setlocale(LC_ALL, "pt_BR", "pt_BR.utf-08", "portuguese");
+
+echo ucwords(strftime("%A %B"));
+
+?>
