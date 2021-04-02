6fd14b5e16fa01e1478217d9a1701467e663a2d9
ArmandoRFilho <armando@mck2.com.br>
2021-04-02 12:10:54 -0300

    Preparando a conexão com PDO

--- PDO/pdo.php
+++ PDO/pdo.php
@@ -1,7 +1,19 @@
-< php
+<?php
 
-$con = new mysqli("localhost", "root", "", "dbphp7" );
-if ($con->connect_error){
-    echo "Error: " . $con->connect_errno;
+$con = new PDO("mysql:dbname=dbphp7;host=localhost", "root","");
 
-?>    
+$stmt = $conn->prepare("SELECT * FROM tb_usuario ORDER BY deslogin");
+
+$stmt->execute();
+
+$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($results as $row) {
+
+    foreach ($row as $key => $value) {
+        
+        echo "<strong>".$key.":</strong>".$value."<br/>";
+    }
+
+    echo "=================================================";
+}
