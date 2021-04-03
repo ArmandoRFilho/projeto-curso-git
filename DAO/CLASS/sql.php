ded83389491975ae47d851cbbf7a0a2ac8736096
ArmandoRFilho <armando@mck2.com.br>
2021-04-03 17:35:29 -0300

    Criando DAO

---
+++ DAO/CLASS/sql.php
@@ -0,0 +1,45 @@
+<?php
+
+class Sql extends PDO
+{
+    private $conn;
+
+    public function __construct()
+    {
+        $this->conn = new PDO("mysql:dbname=dbphp7;host=localhost", "root", "");
+    }
+    private function setParams($statment, $parameters = array())
+    {
+
+        foreach ($parameters as $key => $value) {
+            # code...
+            $this->setParam($key, $value);
+        }
+    }
+
+    private function setParam($statment, $key, $value)
+    {
+
+        $statment->bindParam($key, $value);
+    }
+
+    public function query($rawQuery, $params = array())
+    {
+
+        $stmt = $this->conn->prepare($rawQuery);
+
+        $this->setParams($stmt, $params);
+
+        $stmt->execute();
+
+        return $stmt;
+    }
+
+    public function selecrt($rawQuery, $params = array()): array
+    {
+
+        $this->query($rawQuery, $params);
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
