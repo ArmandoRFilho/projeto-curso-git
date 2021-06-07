74b808c10fbd081345dca683aaaf1b2d4b1cae03
ArmandoRFilho <armando@mck2.com.br>
2021-06-06 23:35:15 -0300

    corrigindo erros de commit

---
+++ FOPEN/exemplo-02.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once("config.php");
+
+$sql = new Sql();
+
+$usuarios = $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin");
+
+$headers = array();
+
+foreach ($usuarios[0] as $key => $value) {
+    # code...
+    array_push($headers, ucfirst($key));
+}
+
+$file = fopen("usuarios.csv", "w+");
+
+fwrite($file, implode(",", $headers) . "\r\n");
+
+foreach ($usuarios as $row) {
+    # code...
+    $data = array();
+
+    foreach ($row as $key => $value) {
+        # code...
+        array_push($data, $value);
+    }//Ende foreach de coluna
+
+    fwrite($file, implode(",", $data) . "\r\n");
+}//End foreach de linha 
+
+fclose($file);
+
+?>
