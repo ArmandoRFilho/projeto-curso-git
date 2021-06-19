b24ac18ed61eae48fe5b7bfe12fd9e840819c0a7
ArmandoRFilho <armando@mck2.com.br>
2021-06-19 20:58:39 -0300

    Criação do FOPEN e UNLINK

--- Aula+71/exemplo-2.php
+++ Aula+71/exemplo-2.php
@@ -6,13 +6,12 @@ $sql = new Sql();
 
 $usuarios = $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin");
 
-print_r($usuarios);
-/*
 $headers = array();
 
 foreach ($usuarios[0] as $key => $value) {
-    # code...
+
     array_push($headers, ucfirst($key));
+
 }
 
 $file = fopen("usuarios.csv", "w+");
@@ -20,17 +19,19 @@ $file = fopen("usuarios.csv", "w+");
 fwrite($file, implode(",", $headers) . "\r\n");
 
 foreach ($usuarios as $row) {
-    # code...
+
     $data = array();
 
     foreach ($row as $key => $value) {
-        # code...
+
         array_push($data, $value);
-    }//Ende foreach de coluna
+
+    }//End Foreach de coluna
 
     fwrite($file, implode(",", $data) . "\r\n");
-}//End foreach de linha 
+
+}//End Foreach de linha
 
 fclose($file);
-*/
+
 ?>
